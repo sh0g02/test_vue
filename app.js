a4e20198bd222a86cff2deba9629d3d9e16523e3
tksk <takasaki@inagora.com>
2021-08-08 00:02:48 +0900

    追加

--- app.js
+++ app.js
@@ -3,7 +3,45 @@ Vue.createApp({
         return {
             todoTitle: '',
             todoDescription: '',
+            todoCategories: [],
+            selectedCategory: '',
+            todos: [],
+            categories: [],
+            hideDoneTodo: false,
             searchWord: '',
+            order: 'desc',
+            categoryName: '',
         }
+    },
+    computed: {
+        canCreateTodo: function () {
+            return this.todoTitle !== '';
+        },
+        canCreateCategory: function () {
+            return this.categoryName !== '' && !this.existCategory();
+        },
+    },
+    methods: {
+        createTodo: function () {
+            if (!this.canCreateTodo) {
+                return;
+            }
+            this.todos.push({
+                // id: 'todo-' +
+            });
+            this.todoTitle = '';
+            this.todoDescription = '';
+            this.todoCategories = [];
+        },
+        createCategory: function () {
+            if (!this.canCreateCategory) {
+                return;
+            }
+            // TODO
+            this.categoryName = '';
+        },
+        existCategory: function () {
+            return this.categories.indexOf(this.categoryName) !== -1;
+        },
     },
 }).mount('#app')
